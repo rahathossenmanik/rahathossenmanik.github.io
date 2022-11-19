e57f0ec258d32a60da6a46eb6efa453a425c0c05
Manik Hosen <rahathossenmanik@gmail.com>
2022-11-19 23:52:17 +0600

    Portfolio Tab Responsive

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,10 @@
-import { Col, Row } from 'reactstrap';
+import { useState } from 'react';
+import { Button, Col, Row } from 'reactstrap';
 import { portfolio1, portfolio2, portfolio3, portfolio4 } from '../assets/img';
 
 const Portfolio = () => {
+	const [tab, setTab] = useState('all');
+
 	return (
 		<section className='py-5 border-bottom wow' data-wow-delay='0.1s'>
 			<h2 className='title pb-3 mb-5 text-start'>Portfolio</h2>
@@ -10,20 +13,32 @@ const Portfolio = () => {
 					<Row>
 						<Col className='text-center mb-2'>
 							<ul className='list-inline mb-4' id='portfolio-flters'>
-								<li className='btn btn-secondary active' data-filter='*'>
+								<Button
+									color='secondary'
+									className={tab === 'all' ? 'active' : ''}
+									onClick={() => setTab('all')}>
 									<i className='fa fa-star me-2'></i>All
-								</li>
-								<li className='btn btn-secondary' data-filter='.first'>
+								</Button>
+								<Button
+									color='secondary'
+									className={tab === 'design' ? 'active' : ''}
+									onClick={() => setTab('design')}>
 									<i className='fa fa-laptop-code me-2'></i>Design
-								</li>
-								<li className='btn btn-secondary' data-filter='.second'>
+								</Button>
+								<Button
+									color='secondary'
+									className={tab === 'dev' ? 'active' : ''}
+									onClick={() => setTab('dev')}>
 									<i className='fa fa-mobile-alt me-2'></i>Development
-								</li>
+								</Button>
 							</ul>
 						</Col>
 					</Row>
 					<Row className='portfolio-container'>
-						<Col md='6' className='mb-4 portfolio-item first'>
+						<Col
+							md='6'
+							className='mb-4 portfolio-item'
+							style={{ display: tab === 'dev' ? 'none' : '' }}>
 							<div className='position-relative overflow-hidden mb-2'>
 								<img className='img-fluid w-100' src={portfolio1} alt='' />
 								<div className='portfolio-btn d-flex align-items-center justify-content-center'>
@@ -33,7 +48,10 @@ const Portfolio = () => {
 								</div>
 							</div>
 						</Col>
-						<Col md='6' className='mb-4 portfolio-item second'>
+						<Col
+							md='6'
+							className='mb-4 portfolio-item'
+							style={{ display: tab === 'dev' ? 'none' : '' }}>
 							<div className='position-relative overflow-hidden mb-2'>
 								<img className='img-fluid w-100' src={portfolio2} alt='' />
 								<div className='portfolio-btn d-flex align-items-center justify-content-center'>
@@ -43,7 +61,10 @@ const Portfolio = () => {
 								</div>
 							</div>
 						</Col>
-						<Col md='6' className='mb-4 portfolio-item first'>
+						<Col
+							md='6'
+							className='mb-4 portfolio-item'
+							style={{ display: tab === 'design' ? 'none' : '' }}>
 							<div className='position-relative overflow-hidden mb-2'>
 								<img className='img-fluid w-100' src={portfolio3} alt='' />
 								<div className='portfolio-btn d-flex align-items-center justify-content-center'>
@@ -53,7 +74,10 @@ const Portfolio = () => {
 								</div>
 							</div>
 						</Col>
-						<Col md='6' className='mb-4 portfolio-item second'>
+						<Col
+							md='6'
+							className='mb-4 portfolio-item'
+							style={{ display: tab === 'design' ? 'none' : '' }}>
 							<div className='position-relative overflow-hidden mb-2'>
 								<img className='img-fluid w-100' src={portfolio4} alt='' />
 								<div className='portfolio-btn d-flex align-items-center justify-content-center'>
